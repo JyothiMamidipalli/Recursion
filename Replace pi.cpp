f517b724dd7fb37dfeb078d001b874f93ef6a34b
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2023-10-09 15:16:47 +0530

    Create Replace pi.cpp

---
+++ Replace pi.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+void ReplacePi(int i,char *s)
+{
+	if(s[i]=='\0' and s[i+1]=='\0')
+	{
+		return ;
+	}
+	if(s[i]=='p' and s[i+1]=='i')
+	{
+		int j=i+2;
+		//go to last
+		while(s[j]!='\0') j++;
+		//shift right
+		while(j >= i+2){
+            s[j+2] = s[j];
+            j--;
+        }
+        s[i]='3';
+        s[i+1]='.';
+        s[i+2]='1';
+        s[i+3]='4';
+        ReplacePi(i+4,s);
+	}
+	else{
+        ReplacePi(i+1,s);
+    }
+}
+int main(){
+  char s[1000] ;
+  cin >> s;
+  //int len = strlen(s);
+  ReplacePi(0,s);
+  cout<<s;
+  return 0;
+}
